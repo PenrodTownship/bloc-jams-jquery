dde3a0238146af76193497a6f88514334ae77f52
Josh Penrod <josh.penrod@startribune.com>
2018-12-07 06:50:37 -0600

    In progress assignment with broken if statement on prevSongIndex

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -14,4 +14,22 @@
     const nextSong = album.songs[nextSongIndex];
     player.playPause(nextSong);
   });
+
+  $('button#previous').on('click', function() {
+    if (player.playState !== 'playing') { return; }
+
+    const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
+    const prevSongIndex = currentSongIndex - 1;
+/* this if statement below is broken; everything else works! */
+/*    if (prevSongIndex = 0) { return; }*/
+
+    const prevSong = album.songs[prevSongIndex];
+    player.playPause(prevSong);
+  });
+
+
 }
+
+/* in the first if statement, why >= instead of just = ? */
+
+/* also, if const is a one-time use, why am I able to call the exact same variable again? */
